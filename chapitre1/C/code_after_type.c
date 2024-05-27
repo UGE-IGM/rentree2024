4065d748fe0f716ac511f09df7ff966ae9ab6dee
Maks <maksmunait@gmail.com>
2024-05-27 16:15:24 +0200

    first_commit

--- chapitre1/C/code_after_type.c
+++ chapitre1/C/code_after_type.c
@@ -3,25 +3,24 @@
 /* code after typique */
 
 int main(int argc,char **argv){
- 
-/* traitement des parametres d'appel avec saut du nom de l'executable */
-while (*++argv)
- {
-    /* parametre entier */
-    int i=atoi(*argv);
-    /* parametre float */
-    float f=atof(*argv);
-    /* parametre chaine */
-    char *s=*argv;
+    int i; float f; char *s;
+    /* traitement des parametres d'appel avec saut du nom de l'executable */
+    while (*++argv){
+        /* parametre entier */
+        i=atoi(*argv);
+        /* parametre float */
+        f=atof(*argv);
+        /* parametre chaine */
+        s = *argv;
 
-    /* faire quelque chose */
-    student_fonction(i,f,s);
-}
+        /* faire quelque chose */
+        student_fonction(i,f,s);
+    }
 
-/** utilisation de l'entrée standard */
-while (scanf("%d %f %s",&i,&f,s)==3)
-{
-    /* faire quelque chose */
-    student_fonction(i,f,s);
-}
+    /** utilisation de l'entrée standard */
+    while (scanf("%d %f %s",&i,&f,s)==3)
+    {
+        /* faire quelque chose */
+        student_fonction(i,f,s);
+    }
 }
