7248ef275c09d16a470647996fa88a51464b9727
Maks <maksmunait@gmail.com>
2024-05-29 13:12:28 +0200

    modifcation expression.py

--- communs/expression.py
+++ communs/expression.py
@@ -6,6 +6,16 @@
 # les constantes sont dans la liste args 
 
 from random import choice,  randint
+from evalC import evalC
+
+def expressionC(op, *args):
+    nbop = randint(2, 3)
+    if nbop == 2:
+        exp  =str(choice(args))+choice(op)+str(choice(args))
+    else:
+        exp = str(choice(args))+choice(op)+str(choice(args))+choice(op)+str(choice(args))
+    return exp,evalC(exp)
+
 
 def expression(op, *args):
     nbop = randint(2, 3)
@@ -23,9 +33,18 @@ def listexpr(nb,op, *args):
     return l
 
 
+def listexpr_C(nb,op, *args):
+    l=[]
+    for i in range(nb):
+        l.append(expression(op,*args))
+    return l
+
 
 # Exemple d'utilisation
 for w in range(10):
     print(expression(['+', '-', '*', '/'], 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 
+
+# Exemple d'utilisation
+print(listexpr_C(10, ['*', '>>', '<<', '|'], 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 
