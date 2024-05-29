c2d7e9d11012bc961acc41d20399b54245d84e3f
nimdanor <dominique.revuz@gmail.com>
2024-05-29 12:02:16 +0200

    Version initial de expression

---
+++ communs/expression.py
@@ -0,0 +1,20 @@
+
+
+
+# Retourne une expresions constante avecaléatoireement 2 ou trois opérandes 
+# Construire avec les operateurs de la liste op
+# les constantes sont dans la liste args 
+
+from random import choice,  randint
+
+def expression(op, *args):
+    nbop = randint(2, 3)
+    if nbop == 2:
+        exp  =str(choice(args))+choice(op)+str(choice(args))
+    else:
+        exp = str(choice(args))+choice(op)+str(choice(args))+choice(op)+str(choice(args))
+    return exp,eval(exp)
+
+# Exemple d'utilisation
+for w in range(10):
+    print(expression(['+', '-', '*', '/'], 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
