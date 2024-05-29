6271b57eb2865bb14c6cb6048d2445964f8a329c
nimdanor <dominique.revuz@gmail.com>
2024-05-29 12:11:05 +0200

    adde elistexpr

--- communs/expression.py
+++ communs/expression.py
@@ -15,6 +15,17 @@ def expression(op, *args):
         exp = str(choice(args))+choice(op)+str(choice(args))+choice(op)+str(choice(args))
     return exp,eval(exp)
 
+
+def listexpr(nb,op, *args):
+    l=[]
+    for i in range(nb):
+        l.append(expression(op,*args))
+    return l
+
+
+
 # Exemple d'utilisation
 for w in range(10):
     print(expression(['+', '-', '*', '/'], 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+
